545079aad16b1c41cc8f10832335fa7cda57b179
Nicholas Tarsis <ntarsis30@stuy.edu>
2022-02-03 09:51:48 -0500

    Finished recursive methods without testing yet

---
+++ 01-recursion/Recursion.java
@@ -0,0 +1,53 @@
+public class Recursion{
+
+    /*Print all words that are made of the letters a-e inclusive.
+    *@param length : the length of the words that are to be printed
+    */
+    public static void printAllWords(int length){
+      printAllWords(length,"");
+    }
+
+    /*Print all words that are made of the letters a-e inclusive.
+    *@param length : either how many more letters or the total length depending on implementation
+    *@param word   : the partial word so far.
+    */
+    public static void printAllWords(int length, String word){
+      if (length==0){
+        System.out.println(word);
+        return;
+      }
+      for (char c = 'a'; c <= 'e';c++){
+        printAllWords(length-1,word+c);
+      }
+    }
+
+    /*Print all words that are made of the characters in the array of letters.
+    *There may not be consecutive equal letters, so:
+    *aax is not allowed, but axa is allowed.
+    *@param length : the length of the words that are to be printed
+    *@param letters: the letters you should be using
+    */
+    public static void printNoDoubleLetterWords(int length,char[] letters){
+      printNoDoubleLetterWords(length,"",letters);
+    }
+
+    /*Print all words that are made of the characters in letters. There may not be consecutive equal letters,
+    *aax is not allowed, but axa is allowed.
+    *@param length : either how many more letters need to be
+    *@param word   : the partial word so far.
+    *@param letters: the letters you should be using
+    */
+    public static void printNoDoubleLetterWords(int length,String word,char[]letters){
+      if (length==0){
+        System.out.println(word);
+        return;
+      }
+      for (char c = 'a'; c <= 'e';c++){
+        if (word.length()>0 && word.charAt(word.length()-1)!=c){
+          printNoDoubleLetterWords(length-1,word+c, letters);
+        }
+      }
+    }
+
+
+}
