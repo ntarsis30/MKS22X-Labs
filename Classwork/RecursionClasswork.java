a0814c076793e642c82e3c2bf86a65a08cadc882
Nicholas Tarsis <ntarsis30@stuy.edu>
2022-02-09 22:35:07 -0500

    RecursionClasswork Finished split array and used tests from site

--- Classwork/RecursionClasswork.java
+++ Classwork/RecursionClasswork.java
@@ -11,10 +11,27 @@ public class RecursionClasswork{
         }
         return false;
     }
+    public static boolean splitArray(int[] nums) {
+        int half_sum = 0;
+        for (int i : nums){
+            half_sum+=i;
+        }
+        if(half_sum%2==1){
+            return false;
+        }
+        return groupSum(0,nums,half_sum/2);
+    }
+
     public static void main(String[] args){
         int[] test = {8,4,2};
+        int[] test1 = {2,2};
+        int[] test2 = {2,3};        
+        int[] test3 = {5,2,3};
         System.out.println(groupSum(0,test,10));
         System.out.println(groupSum(0,test,14));
         System.out.println(groupSum(0,test,9));
+        System.out.println(splitArray(test1));
+        System.out.println(splitArray(test2));
+        System.out.println(splitArray(test3));
     }
 }
