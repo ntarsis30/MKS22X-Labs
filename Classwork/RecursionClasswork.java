5b8fba6b55198baf8d7b2be280283b29e67118e5
Nicholas Tarsis <ntarsis30@stuy.edu>
2022-02-09 23:15:54 -0500

    RecursionClasswork Made slight changes to create groupsum6

--- Classwork/RecursionClasswork.java
+++ Classwork/RecursionClasswork.java
@@ -21,6 +21,24 @@ public class RecursionClasswork{
         }
         return groupSum(0,nums,half_sum/2);
     }
+    public static boolean groupSum6(int start, int[] nums, int target) {
+        if(target==0 && start==nums.length){
+            return true;
+        }
+        if (target<0){
+            return false;
+        }
+        if (start<nums.length && nums[start]!=6){
+            return groupSum(start+1, nums, target-nums[start])||groupSum(start+1, nums, target);
+        }
+        else if(start < nums.length){
+            return groupSum(start+1, nums, target-nums[start]);
+        }
+        return false;
+
+  
+    }
+
 
     public static void main(String[] args){
         int[] test = {8,4,2};
@@ -30,8 +48,13 @@ public class RecursionClasswork{
         System.out.println(groupSum(0,test,10));
         System.out.println(groupSum(0,test,14));
         System.out.println(groupSum(0,test,9));
+
         System.out.println(splitArray(test1));
         System.out.println(splitArray(test2));
         System.out.println(splitArray(test3));
+
+        System.out.println(groupSum(0,test,10));
+        System.out.println(groupSum(0,test,14));
+        System.out.println(groupSum(0,test,9));
     }
 }
