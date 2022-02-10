932e4aa14b5ac5b0921a4129ee3b620c9b3117ef
Nicholas Tarsis <ntarsis30@stuy.edu>
2022-02-10 00:43:37 -0500

    RecursionClasswork Created method outline for groupsum5

--- Classwork/RecursionClasswork.java
+++ Classwork/RecursionClasswork.java
@@ -74,6 +74,16 @@ public class RecursionClasswork{
         }
         return false;
     }
+    public boolean groupSum5(int start, int[] nums, int target) {
+        if(start>=nums.length){
+            return target == 0;
+        }
+        if (nums[start]==6){
+            return groupSum5(start+2, nums, target-nums[start]);
+        }
+        return groupSum5(start+1, nums, target-nums[start])||groupSum5(start+1, nums, target);
+  
+    }
 
 
 
