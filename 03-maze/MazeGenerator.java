ba62948caa897b4ff5731349ff3b47fec57f0d2e
Nicholas Tarsis <ntarsis30@stuy.edu>
2022-03-07 09:40:50 -0500

    Removed print statements

--- 03-maze/MazeGenerator.java
+++ 03-maze/MazeGenerator.java
@@ -35,11 +35,9 @@ public class MazeGenerator{
     public static void recurse(char[][]maze, int startrow, int startcol){
         if (!safe(maze,startrow,startcol) && maze[startrow][startcol]!=' '){
             maze[startrow][startcol]='.';
-            System.out.println(toString(maze));
             return;
         }
         maze[startrow][startcol]=' ';
-        System.out.println(toString(maze));
         int[] dir1 = new int[] {1,-1,0,0};
         int[] dir2 = new int[] {0,0,1,-1};
         int[] order = permRand();
