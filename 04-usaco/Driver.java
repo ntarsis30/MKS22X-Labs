3a6c3f666b85a362eccf495c2056d47a26ee5ae6
Nicholas Tarsis <ntarsis30@stuy.edu>
2022-03-15 23:46:11 -0400

    Checked exception handling

--- 04-usaco/Driver.java
+++ 04-usaco/Driver.java
@@ -1,6 +1,8 @@
 public class Driver{
     public static void main(String[] args){
+        //long ans = Bronze.solve("lake.input");
         long ans = Bronze.solve("lake1.input");
+
         System.out.println(ans);
     }
 }
