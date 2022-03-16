aa9d4aaf9ebb3766b747e009f55c73e6cc77d50d
Nicholas Tarsis <ntarsis30@stuy.edu>
2022-03-16 00:12:12 -0400

    Checked extra test and exceptions again

--- 04-usaco/Driver.java
+++ 04-usaco/Driver.java
@@ -1,9 +1,11 @@
 public class Driver{
     public static void main(String[] args){
         //long ans = Bronze.solve("lake.input");
-        //long ans = Bronze.solve("lake1.input");
-        long ans = Silver.solve("cowPath1.input");
+        long ans = Bronze.solve("lake1.input");
+        System.out.println(ans);
 
+        //long ans = Silver.solve("cowPath.input");
+        ans = Silver.solve("cowPath1.input");
         System.out.println(ans);
     }
 }
