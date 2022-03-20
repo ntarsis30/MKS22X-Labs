fa9c66a81ebd6e77db39fa8ad0c22051d82fd9d7
Nicholas Tarsis <ntarsis30@stuy.edu>
2022-03-20 16:03:06 -0400

    Finished implementing the dutch partition and testing

--- 05-quick/Quick.java
+++ 05-quick/Quick.java
@@ -9,7 +9,7 @@ public class Quick{
         int new_pivot = start-1;
         boolean right = true;
         for (int i = start; i < end; i++){
-            if(data[i] <= data[end]){
+            if (data[i] <= data[end]){
                 if (data[i]==data[end] && right){
                     //already on the right of new_pivot, so no swap
                     right = false;
@@ -28,6 +28,37 @@ public class Quick{
         data[end] = fix;
         return new_pivot+1;
     }
+    public static int[] partitionDutch(int[] data,int lo, int hi){
+        Random rand = new Random();
+        int pivot = rand.nextInt(hi-lo+1) + lo;
+        int val = data[pivot];
+        int temp = data[hi];
+        data[hi] = data[pivot];
+        data[pivot] = temp;
+        int new_pivot = lo;
+        while (new_pivot <= hi){
+            if (data[new_pivot] < val){
+                int swap = data[lo];
+                data[lo] = data[new_pivot];
+                data[new_pivot] = swap;
+                lo++;
+                new_pivot++;
+            }
+            else if (data[new_pivot] > val){
+                int swap = data[new_pivot];
+                data[new_pivot] = data[hi];
+                data[hi] = swap;
+                hi--;
+            }
+            else{
+                new_pivot++;
+            }
+        }
+    int[] ans = new int[] {lo-1,new_pivot};
+    return ans;
+    }
+
+    
     public static int quickselect(int[] data, int k){
         int lo = 0;
         int hi = data.length-1;
@@ -50,25 +81,27 @@ public class Quick{
     }
     public static void quicksort(int[] data,int lo,int hi){
         //your code.
-        if(lo < hi){
-            int pivot = partition(data, lo, hi);
-            quicksort(data, lo, pivot-1);
-            quicksort(data, pivot+1, hi);
+        if (lo < hi){
+            int[] pivot = partitionDutch(data, lo, hi);
+            quicksort(data, lo, pivot[0]);
+            quicksort(data, pivot[1], hi);
         }
     }
     public static void main(String[] args){
-        //int [] data = new int[] {4,3,2,1,0};
+        int [] data = new int[] {4,3,2,1,0};
         //int [] data = new int[] {4,3,2,1,0,997,998,999};
         //int [] data = new int[] {997,998,999,4,3,2,1,0};
-        int[] data = new int[] {1,1,1,1,1,1,1,1,1,1};
+        //int[] data = new int[] {1,1,1,1,1,1,1,1,1,1};
         System.out.println("Original: "+Arrays.toString(data));
         for (int i = 0; i < data.length; i++){
-            System.out.println(quickselect(data,i));
+            //System.out.println(quickselect(data,i));
         }
-        //int pivot = partition( data , 0, 4);
+        quicksort(data);
+
+        int pivot[] = partitionDutch( data , 0, 4);
         //int pivot = partition( data , 3, 7);
-        //System.out.println("Pivot value: "+data[pivot]+ ", Pivot index: "+pivot);
-        //System.out.println("Modified: "+Arrays.toString(data));
+        //System.out.println(pivot[0] + " " + pivot[1]  + " " + pivot[2]);
+        System.out.println("Modified: "+Arrays.toString(data));
 
         System.out.println();
     }
